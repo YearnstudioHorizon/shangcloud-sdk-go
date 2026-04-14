ada90b866de6957510983ee78feaa93358112cc2
YearnstudioYangyi <yangyi@yearnstudio.cn>
2026-04-14 16:35:36 +0800

    feat: 增加rand封装

---
+++ rand.go
@@ -0,0 +1,15 @@
+package shangcloudsdkgo
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func generateRandomString(length int) string {
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(randomBytes)[:length]
+}
